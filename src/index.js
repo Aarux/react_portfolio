e541646080d50f10fe81d42239d892534c46b460
Aarux <gustav_bergman95@hotmail.com>
2020-01-08 09:34:52 +0100

    added lobster and roboto fonts npm package

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App/App";
 import * as serviceWorker from "./serviceWorker";
+require("typeface-roboto");
+require("typeface-lobster");
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
