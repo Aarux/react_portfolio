f59b4801bf8e5ef423687d5297ecb1724689845a
Aarux <gustav_bergman95@hotmail.com>
2020-01-08 09:42:05 +0100

    Update

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,5 +1,7 @@
 import { createGlobalStyle } from "styled-components";
 import styled from "styled-components";
+import "typeface-roboto";
+import "typeface-lobster";
 
 /* ------------------------------------------------ */
 /* ------------------------------------------------ */
